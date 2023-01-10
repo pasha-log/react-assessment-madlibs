0f6d714c488a9c3dae2e588d0a2103ff0f826468
pasha-log <pasha.log@gmail.com>
2023-01-09 22:24:16 -0800

    solved addCommas function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,32 @@
-function addCommas() {}
+const addCommas = (num) => {
+
+// This was my initial try by my own logic: 
+// // turn num into string so it can be more mutable.
+// let numAsStr = num.toString();
+
+// // eliminate three digit numbers that are negative or positive right off the bat
+// if ((numAsStr[0] === "-" && numAsStr.length <= 4) || (numAsStr.length <= 3)) {
+//     return numAsStr;
+// }
+
+// https://stackoverflow.com/questions/6784894/add-commas-or-spaces-to-group-every-three-digits
+
+// This turns num into a string in an array called str, and if num contains a decimal it splits into two strings
+// let str = num.toString().split('.');
+
+// I don't understand this unicode logic very much:
+// if (str[0].length >= 5) {
+//     str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+// }
+// if (str[1] && str[1].length >= 5) {
+//     str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+// }
+// return str.join('.');
+
+// Or simply this can be used: 
+return num.toLocaleString();
+
+// https://stackoverflow.com/questions/6784894/add-commas-or-spaces-to-group-every-three-digits
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,34 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test("works for 1234", () => {
+    expect(addCommas(1234)).toBe("1,234");
+  })
+  test("works for 1000000", () => {
+    expect(addCommas(1000000)).toBe("1,000,000");
+  })
+  test("works for 9876543210", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  })
+  test("works for 6", () => {
+    expect(addCommas(6)).toBe("6");
+  })
+  test("works for -10", () => {
+    expect(addCommas(-10)).toBe("-10");
+  })
+  test("works for -5678", () => {
+    expect(addCommas(-5678)).toBe("-5,678");
+  })
+  test("works for -76125678", () => {
+    expect(addCommas(-76125678)).toBe("-76,125,678");
+  })
+  test("works for 96425231", () => {
+    expect(addCommas(96425231)).toBe("96,425,231");
+  })
+  test("works for 137425231", () => {
+    expect(addCommas(137425231)).toBe("137,425,231");
+  })
+  test("works for 423638", () => {
+    expect(addCommas(423638)).toBe("423,638");
+  })
 });
